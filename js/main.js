6081dc5b06c3a02718315c0f9bba0c6ae2d15c50
s_murakami <s.murakami@conol.jp>
2014-10-16 00:01:52 +0900

    アニメートするようにした

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 $(function(){
+  jQuery.extend(jQuery.easing, {
+    easeOutExpo: function (x, t, b, c, d) {
+      return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+    }
+  });
+
+  // 最初のアニメーション
+  $('#main').css({
+    marginLeft: '-100%'
+  });
+  $('#main').animate({
+    marginLeft: '0'
+  }, 500, 'easeOutExpo');
+
+  // クリックされたらアニメーション
   $('#nav .link a').click(function(e){
     e.preventDefault();
-    window.location.href = e.target.parentNode.href;
+    $('#main').animate({
+      marginLeft: '-100%'
+    }, 500, 'easeOutExpo', function(){
+      window.location.href = e.target.parentNode.href;
+    });
   });
 });
+
 
