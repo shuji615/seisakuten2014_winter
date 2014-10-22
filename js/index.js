bb407ce5b5c1e09b72fe92a260394996cc89a943
s_murakami <s.murakami@conol.jp>
2014-10-22 12:17:52 +0900

    サイズの変更を実装

--- js/index.js
+++ js/index.js
@@ -18,7 +18,29 @@ $(function(){
       $('.sprite').removeClass(imageClasses[i]);
     }
     $('.sprite').addClass(imageClasses[counter % imageClasses.length]);
-    console.log('hello');
+
+    // サイズ
+    var size = counter % 3 == 0 ? 'lg-size' : 'md-size';
+    var prevSize = (counter - 1) % 3 == 0 ? 'lg-size' : 'md-size';
+    $('.sprite').removeClass(prevSize);
+    $('.sprite').addClass(size);
+
+    var marginLeft = counter % 2 == 0 ? '100px' : '0px';
+    $('.sprite').animate({
+      marginLeft: marginLeft,
+    }, {
+      queue: false,
+      duration: 500,
+      easing: 'easeOutElastic'
+    });
+    // $('.sprite').animate({
+    //   width: size,
+    //   height: size,
+    // }, {
+    //   queue: false,
+    //   duration: 500,
+    // });
+
     counter++;
   }, 1000);
 
